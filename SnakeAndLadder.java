03fdf3783101df7693396d045fba8085cf27d684
PrachitiP <patilprachiti35@gmail.com>
2020-11-18 17:41:19 +0530

    Snake and Ladder

---
+++ SnakeAndLadder.java
@@ -0,0 +1,15 @@
+package BridgeLabz.SnakeAndLadder;
+
+public class SnakeAndLadder {
+	
+	public static int startPos = 0;
+	public static int player = 1;
+	public static int winningPoints = 100;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Snake And Ladder");
+	}
+
+}
